3b7315eeeb1936009c16e5bdee1263270a43203d
Beilong <bt132@duke.edu>
2025-02-21 18:35:34 +0800

    update

--- rest_server/server_app.py
+++ rest_server/server_app.py
@@ -3,10 +3,40 @@ import sys
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 from flask import Flask, request, jsonify
-from rules_generate.model_wrapper import Model
+import pickle
+import random
+import itertools
+from typing import List
 
+class Model:
 
-
+    def __init__(self, rules_ckpt:str):
+        self.model = pickle.load(open(rules_ckpt, "rb"))
+    
+    def _check_exist(self, data):
+        for _e in self.model:
+            if _e[0] == set(data):
+                return list(_e[1])
+        return None
+    
+    def __call__(self, data: List[str]):
+        """
+        data: List of songs
+        """
+        ret_val = []
+        ## Iterate through the data 
+        for r in range(len(data), 0, -1):
+            combinations = list(itertools.combinations(data, r))
+            for comb in combinations:
+                for _e in self.model:
+                    if all(elem in list(_e[0]) for elem in list(comb)):
+                        # return list(_e[1])
+                        for e in list(_e[1]):
+                            if e not in ret_val:
+                                ret_val.append(e)
+        if len(ret_val) != 0:
+            return ret_val
+        return list(random.choice(self.model)[1])
 
 app = Flask(__name__)
 app.model = Model("../rules_generate/ckpt/sup_0.025_conf_0.7_rules.ckpt")
