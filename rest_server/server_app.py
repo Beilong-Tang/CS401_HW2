2dc33e7d9f5e8e07ce7f7e45af480a867a69fa78
Beilong <bt132@duke.edu>
2025-02-21 18:47:39 +0800

    update

--- rest_server/server_app.py
+++ rest_server/server_app.py
@@ -1,7 +1,6 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
-
 from flask import Flask, request, jsonify
 import pickle
 import random
@@ -43,9 +42,7 @@ app.model = Model("ckpt/sup_0.025_conf_0.7_rules.ckpt")
 
 @app.route("/api/recommender", methods=["POST"])
 def recommend():
-    print(request)
     value = request.get_json()
-    print(value)
     res = app.model(value['songs'])
     return jsonify({"recommend": res})
 
