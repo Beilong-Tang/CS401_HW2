d69d10439e7346d801707c6432bec8abfc829a3d
Beilong <bt132@duke.edu>
2025-02-22 20:54:51 +0800

    update

--- rest_client/web_app.py
+++ rest_client/web_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, render_template
+import requests
 
 import argparse
 p = argparse.ArgumentParser()
@@ -11,6 +12,14 @@ app = Flask("client web app")
 @app.route("/")
 def main():
     return render_template("index.html", hostname=args.hostname)
+
+@app.route("/get_recommendation", methods = ["POST"])
+def get_recommendation():
+    value = request.get_json() # the return value
+    url = f"http://{args.hostname}:52006/api/recommender"
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, json=value, headers=headers)
+    print(response.text)
 
 
 app.run('0.0.0.0', port = args.web_port)
