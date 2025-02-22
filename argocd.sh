3289779ef712f9aef2f453c69cafc9b34e0db985
bt132@duke.edu <bt132@duke.edu>
2025-02-22 03:48:51 -0700

    u

--- argocd.sh
+++ argocd.sh
@@ -1,6 +1,6 @@
 ## This scripts serves as the purpose to create argoCD
 
-argocd app create bl_cs401_hw2 \
+argocd app create bl-cs401-hw2 \
       --repo https://github.com/Beilong-Tang/CS401_HW2.git \
       --path . \
       --project beilong-project \
