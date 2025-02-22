440f7fb7ead5341a01f89cf2c9d4ecc7b489c930
bt132@duke.edu <bt132@duke.edu>
2025-02-22 07:37:36 -0700

    u

---
+++ test/keep_request.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cur_path=$(pwd)
+
+cd ../rest_client
+
+# Define the log file
+LOGFILE="test_dataset_update.log"
+
+> "$cur_path/$LOGFILE"
+
+# Define the interval (in seconds) between each execution
+INTERVAL=1
+
+while true; do
+    # Get the current timestamp
+    TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+
+    # Run the kubectl command and capture the output
+    OUTPUT=$(python client_cli.py --songs 'Portland' --hostname 10.109.101.236)
+
+    # Append the timestamp and output to the log file
+    echo "[$TIMESTAMP]" >> "$cur_path/$LOGFILE"
+    echo "$OUTPUT" >> "$cur_path/$LOGFILE"
+    echo "-------------------" >> "$LOGFILE"
+
+    # Wait for the specified interval before running again
+    sleep "$INTERVAL"
+done
